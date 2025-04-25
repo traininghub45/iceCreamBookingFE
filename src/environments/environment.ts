bc47704222f6edba7f9f19012d91e4bdac21fdc5
Mohammad Eid <mohdeid2000@gmail.com>
2025-04-26 00:34:49 +0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api',
+    apiUrl: 'http://localhost:5000/api',
     jwtTokenKey: 'access_token',
     imageUrl: 'http://localhost:5000',
   };
